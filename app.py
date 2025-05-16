a53cc2d5f4629e90a1071c3cf535decd659faac1
Archit Yadav <127927443+ArchitYad@users.noreply.github.com>
2025-05-16 16:06:51 +0530

    Update app.py changes in attention layer

--- app.py
+++ app.py
@@ -8,10 +8,12 @@ from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 from tensorflow.keras.models import load_model
 from tensorflow.keras.layers import Layer
+from tensorflow.keras.utils import register_keras_serializable
 
+@register_keras_serializable()
 class AttentionLayer(Layer):
-    def __init__(self):
-        super(AttentionLayer, self).__init__()
+    def __init__(self, **kwargs):
+        super(AttentionLayer, self).__init__(**kwargs)
 
     def build(self, input_shape):
         self.W = self.add_weight(shape=(input_shape[-1], 1),
@@ -28,30 +30,33 @@ class AttentionLayer(Layer):
         context_vector = tf.reduce_sum(context_vector, axis=1)
         return context_vector
 
-@st.cache_resource
-def load_model_with_attention():
-    return load_model('models/model.h5', custom_objects={'AttentionLayer': AttentionLayer})
-
-with st.spinner("Loading model..."):
-    model = load_model_with_attention()
+    def get_config(self):
+        config = super(AttentionLayer, self).get_config()
+        return config
 
 with open('label_map.json', 'r') as f:
     label_map = json.load(f)
-label_decoder = {int(v): k for k, v in label_map.items()}
+label_decoder = {v: k for k, v in label_map.items()}
 
 with open('tokenizer.pkl', 'rb') as f:
     tokenizer = pickle.load(f)
 
+@st.cache_resource
+def load_model_with_attention():
+    return load_model('models/model.h5', custom_objects={'AttentionLayer': AttentionLayer})
+
+with st.spinner("Loading Model..."):
+    model = load_model_with_attention()
+
 def clean_text(text):
     text = str(text).lower()
-    text = re.sub(r"http\S+|www\S+|https\S+", '', text)
-    text = re.sub(r'\@\w+|\#', '', text)
+    text = re.sub(r"http\S+|www\S+|https\S+", '', text, flags=re.MULTILINE)
+    text = re.sub(r'\@\w+|\#','', text)
     text = re.sub(r"[^a-zA-Z\s]", "", text)
     text = re.sub(r"\s+", " ", text).strip()
     return text
 
 st.title("🧠 Mental Health Text Classifier")
-
 user_input = st.text_area("Enter your mental health-related text")
 
 if st.button("Predict"):
@@ -63,5 +68,5 @@ if st.button("Predict"):
         padded = pad_sequences(seq, maxlen=30, padding='post')
         prediction = model.predict(padded)
         predicted_label_index = np.argmax(prediction)
-        predicted_label = label_decoder[predicted_label_index]
+        predicted_label = label_decoder[str(predicted_label_index)]
         st.success(f"Predicted Label: `{predicted_label}`")
